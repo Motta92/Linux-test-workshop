f12a5343a5a8de66cbb96b36fafa8ce67b9d0f07
Motta92 <Carlos93504@hotmail.com>
2013-09-05 19:50:36 -0500

    resta y mult actualizados

--- mult.sh
+++ mult.sh
@@ -1,6 +1,7 @@
-!#/bin/bash
+#!/bin/bash
+
 function mult(){
-echo $(($1 * $2))
+echo "$1 * $2" | bc -l
 }
 
 mult $1 $2
--- resta.sh
+++ resta.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 function resta(){
-echo $(($1 - $2))
+echo "$1 - $2" | bc -l
 }
 
 resta $1 $2
